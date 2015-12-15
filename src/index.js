5121583f2b9e24184b642fe2f1f1abe4833a7b0c
Enrico Hoffmann <dasrick@gmail.com>
2015-12-15 16:47:17 +0100

    add a lot of inital stuff ...

---
+++ src/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngInject
+ */
+//var AlertService = require('./AlertService'),
+//    StateChangeErrorHandler = require('./StateChangeErrorHandler'),
+//    ResponseErrorInterceptor = require('./ResponseErrorInterceptor');
+
+module.exports = angular
+  .module('mi.Chat', [])
+
+  //.provider('StateChangeErrorHandler', StateChangeErrorHandler)
+  //.provider('ResponseErrorInterceptor', ResponseErrorInterceptor)
+  //
+  //.service('AlertService', AlertService)
+  //.constant('ALERT_LEVELS', {
+  //  danger: {timeout: 5000},
+  //  warning: {timeout: 4000},
+  //  success: {timeout: 3000},
+  //  info: {timeout: 2000}
+  //})
+;
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// modules /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var webpack = require('webpack');
+var path = require('path');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+// pathes //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var srcPath = path.resolve(__dirname, 'src', 'index.js');
+var dstPath = path.resolve(__dirname, 'dist');
+
+// the base config /////////////////////////////////////////////////////////////////////////////////////////////////////
+var config = {
+  devtool: 'inline-source-map',
+  entry: srcPath,
+  output: {
+    path: dstPath,
+    filename: 'mi-angular-chat.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  plugins: [
+    new ngAnnotatePlugin({add: true}),
+    //new webpack.optimize.UglifyJsPlugin(
+    //  {compress: {warnings: false}}
+    //),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin()
+  ]
+};
+
+module.exports = config;
